d60f71774afa7b69d422b7ad91be8b143feb6cf5
AntonyBlanco <ablancot@unsa.edu.pe>
2024-03-13 13:56:26 -0500

    Debugging Location problem infinite

--- Assets/Scripts/Main Game 2/PlayerController.cs
+++ Assets/Scripts/Main Game 2/PlayerController.cs
@@ -37,6 +37,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Game started");
         smoke = GetComponentInChildren<ParticleSystem>();
         emission = smoke.emission;
         carAudio = GetComponent<AudioSource>();
@@ -53,30 +54,33 @@ public class PlayerController : MonoBehaviour
         helper.OnCharacteristicChanged += OnCharacteristicChanged;
         helper.OnCharacteristicNotFound += OnCharacteristicNotFound;
         helper.OnServiceNotFound += OnServiceNotFound;
-        helper.OnCharacteristicChanged += (helper, value, characteristic) =>
-        {
-            caption = Encoding.Unicode.GetString(value);
-        };
+        //helper.OnCharacteristicChanged += (helper, value, characteristic) =>
+        //{
+        //    Debug.Log(value);
+        //};
         helper.ScanNearbyDevices();
-
         Permission.RequestUserPermission(Permission.CoarseLocation);
         bluetoothHelperCharacteristic = new BluetoothHelperCharacteristic(characteristicUUIDfxMiddle, serviceUUID);
-        helper.ReadCharacteristic(bluetoothHelperCharacteristic);
+        //caption = helper.Read();
+        //helper.ReadCharacteristic(bluetoothHelperCharacteristic);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Update");
         if (alive)
             Move();
 
         // remove this
         if (Input.GetKeyDown(KeyCode.F7))
             Time.timeScale = 2.5f;
+        Debug.Log("-Update");
     }
 
     void Move()
     {
+        Debug.Log("Move");
         if (Input.GetKeyDown(KeyCode.D))
         {
             currentColumn = 0;
@@ -95,39 +99,48 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.position = positions[currentColumn];
+        Debug.Log("-Move");
     }
 
     public bool Hurt()
     {
+        Debug.Log("Hurt");
         health--;
         if (health <= 0)
         {
             alive = false;
             GameSettings.ScrollSpeed = 0;
+            Debug.Log("-HurtTrue");
             return true;
         }
         emission.rateOverTime = MAX_SMOKE / health;
         carAudio.pitch = basePitch - (5 - health) * 0.1f;
         smoke.Play();
+        Debug.Log("-HurtFalse");
         return false;
     }
 
     public void Score(float points)
     {
+        Debug.Log("Score");
         score += points;
-        scoreText.text = "Puntaje\n" + score + "Caption: " + caption;
+        scoreText.text = "Puntaje\n" + score + " " + caption;
+        Debug.Log("-Score");
     }
 
     void OnScanEnded(BluetoothHelper helper, LinkedList<BluetoothDevice> devices)
     {
+        Debug.Log("OnScanEnded");
         if (helper.isDevicePaired())
             helper.Connect();
         else
             helper.ScanNearbyDevices();
+        Debug.Log("-OnScanEnded");
     }
 
     void OnConnected(BluetoothHelper helper)
     {
+        Debug.Log("OnConnected");
         List<BluetoothHelperService> services = helper.getGattServices();
         foreach (BluetoothHelperService s in services)
         {
@@ -138,43 +151,55 @@ public class PlayerController : MonoBehaviour
             }
         }
         helper.Subscribe(bluetoothHelperCharacteristic);
+        Debug.Log("-OnConnected");
     }
 
     void OnConnectionFailed(BluetoothHelper helper)
     {
+        Debug.Log("OnConnectionFailed");
         Debug.Log("Connection failed");
         helper.ScanNearbyDevices();
+        Debug.Log("-OnConnectionFailed");
     }
 
     void OnCharacteristicChanged(BluetoothHelper helper, byte[] data, BluetoothHelperCharacteristic characteristic)
     {
+        Debug.Log("OnCharacteristicChanged");
         Debug.Log($"Update valud for characteristic [{characteristic.getName()}] of service [{characteristic.getService()}]");
         Debug.Log($"New value : [{System.Text.Encoding.ASCII.GetString(data)}]");
+        Debug.Log("-OnCharacteristicChanged");
     }
 
     void OnServiceNotFound(BluetoothHelper helper, string service)
     {
+        Debug.Log("OnServiceNotFound");
         Debug.Log($"Service [{service}] not found");
+        Debug.Log("-OnServiceNotFound");
     }
 
     void OnCharacteristicNotFound(BluetoothHelper helper, string service, string characteristic)
     {
+        Debug.Log("OnCharacteristicNotFound");
         Debug.Log($"Characteristic [{service}] of service [{service}] not found");
+        Debug.Log("-OnCharacteristicNotFound");
     }
 
     public void Write(string data)
     {
+        Debug.Log("Write");
         helper.WriteCharacteristic(bluetoothHelperCharacteristic, data);
+        Debug.Log("-Write");
     }
 
     void OnDestroy()
     {
-        helper.OnScanEnded -= OnScanEnded;
-        helper.OnConnected -= OnConnected;
-        helper.OnConnectionFailed -= OnConnectionFailed;
-        helper.OnCharacteristicChanged -= OnCharacteristicChanged;
-        helper.OnCharacteristicNotFound -= OnCharacteristicNotFound;
-        helper.OnServiceNotFound -= OnServiceNotFound;
-        helper.Disconnect();
+        Debug.Log("OnDestroy");
+        //helper.OnScanEnded -= OnScanEnded;
+        //helper.OnConnected -= OnConnected;
+        //helper.OnConnectionFailed -= OnConnectionFailed;
+        //helper.OnCharacteristicChanged -= OnCharacteristicChanged;
+        //helper.OnCharacteristicNotFound -= OnCharacteristicNotFound;
+        //helper.OnServiceNotFound -= OnServiceNotFound;
+        //helper.Disconnect();
     }
 }
